2aa546852166b8bf6edc8c4a3ef4f3b006734e6b
Kenny Do <contact@kennydo.com>
2018-02-03 03:58:04 -0800

    Set up server and front-end

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+var request = require('request');
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {}
+    }
+  }
+  componentDidMount() {
+    fetch("http://localhost:8080/testdata")
+    .then(results => results.json())
+    .then(data => this.setState({data}));
+  }
   render() {
+    console.log(this.state);
     return (
       <div className="App">
         <header className="App-header">
@@ -11,7 +24,10 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {
+            Object.keys(this.state.data).length !== 0 &&
+              this.state.data.businesses[0].name
+          }
         </p>
       </div>
     );
