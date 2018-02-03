3f710e9736a6ba86f609dc726185994e8cabec04
Victor Kim <musictrickityking@gmail.com>
2018-02-03 14:47:57 -0800

    Added more values o Business.js, added more css parameters

--- src/components/Business.js
+++ src/components/Business.js
@@ -1,13 +1,38 @@
-import React, { Component } from 'react';
-import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import React from 'react';
+import { ListGroupItem } from 'react-bootstrap';
 
 const Business = (props) => {
-  const {businesses} = props;
+  const {businesses, index} = props;
   return (
-    <ListGroupItem header={businesses.name}>Some body text</ListGroupItem>
+    <div>
+    
+    <span>{index+1}</span>  
+    
+    <ListGroupItem header={businesses.name}>
+      <ul className ="flex-container">
+      <a href = {businesses.url}>
+      <img className = "flex-item" src={businesses.image_url}/>
+      </a>
+      <div className = "paddingtop">
+        <div className = " leftadjust" >{`Rating: ${businesses.rating}`}</div>
+        <div className = " leftadjust">{`Price: ${businesses.price}`}</div>
+        <div>{`Location: ${businesses.location.address1} ${businesses.location.city}, ${businesses.location.state}`}</div>
+        <div className = "leftadjust">{`Distance: ${Math.round(businesses.distance * 3.28084)} Feet`}</div>
+        <img className = "leftadjust im favoritemargin "src={`https://image.flaticon.com/icons/png/128/126/126471.png`}/>
+      </div>
+      </ul>
+    
+    </ListGroupItem>
+    </div>
   )
 }
 
 export default Business;
 
 
+
+//rating
+//$$
+//Favorites
+//Pic of item from menu
+//<div className = "leftadjust">{`Phone #: ${businesses.phone}`}</div>
