1c8b84f70faa23131da416a93057c52167b104f6
Kenny Do <contact@kennydo.com>
2018-02-04 08:23:43 -0800

    Polish EditMealPlan.js and Home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,47 +1,77 @@
 import React from "react";
-import { ListGroup, Button, ButtonToolbar } from 'react-bootstrap';
+import { ListGroup, Button, ButtonToolbar, Alert, Grid, Row, Col } from 'react-bootstrap';
 import Business from "./Business.js";
+
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: {}
+      data: {},
+      error: ""
     }
   }
   componentDidMount() {
     fetch(`/testData`)
     .then(results => results.json())
-    .then(data => this.setState({data}));
+    .then(data => this.setState({data}))
+    .catch(error => this.setState({error: "An error has occured. Sorry!"}));
+  }
+
+  getDateString() {
+    const today = new Date();
+    return `${today.getDate()}/${today.getMonth()+1}`
   }
 
 
 
   render() {
-    const {data} = this.state;
+    const {data, error} = this.state;
+    if (error !== "") {
+      return (
+        <Grid>
+          <Row>
+            <Col>
+            <Alert bsStyle="danger">{error}</Alert>
+            </Col>
+          </Row>
 
+        </Grid>
+
+      )
+    }
     return (
-      <ListGroup> <h1 style={{marginLeft: "200px"}}>Places you can go</h1>
-      <table style={{marginLeft: "20px", width:"80%"}}>
-       <tr>
-         <th>Date</th>
-         <th>Calorie Limit</th>
-         <th>Budget</th>
-       </tr>
-       <tr>
-         <td>12/12/12</td>
-         <td>{localStorage.getItem("calorie") === null ? 0: localStorage.getItem("calorie")}</td>
-         <td>{localStorage.getItem("budget") === null ? 0 : localStorage.getItem("budget")}</td>
-       </tr>
-      </table>
-      <ButtonToolbar>
-       <Button bsStyle="danger" href={Object.keys(data).length !== 0 ? data.businesses[Math.floor(Math.random() * data.businesses.length)].url : ""} style={{marginLeft: "800px"}}>{"I Don't Know where to eat"}</Button>
-       </ButtonToolbar>
-        {
-          Object.keys(data).length !== 0 &&
-            data.businesses.map((element, index) => <Business index={index} businesses={element}/>)
-        }
-      </ListGroup>
+       <Grid>
+        <Row>
+          <Col>
+          <table style={{marginLeft: "20px", width:"80%"}}>
+           <tr>
+             <th>Date</th>
+             <th>Calorie Limit</th>
+             <th>Budget</th>
+           </tr>
+           <tr>
+             <td>{this.getDateString()}</td>
+             <td>{localStorage.getItem("calorie") === null ? 0: localStorage.getItem("calorie")}</td>
+             <td>{`$${localStorage.getItem("budget") === null ? 0 : localStorage.getItem("budget")}`}</td>
+           </tr>
+           <tr>
+             <td></td>
+             <td><Button bsStyle="danger" href={Object.keys(data).length !== 0 ? data.businesses[Math.floor(Math.random() * data.businesses.length)].url : ""}>{"I Don't Know where to eat"}</Button>
+              </td>
+             <td></td>
+           </tr>
+          </table>
+          <ListGroup>
+            {
+              Object.keys(data).length !== 0 &&
+                data.businesses.map((element, index) => <Business index={index} businesses={element}/>)
+            }
+          </ListGroup>
+          </Col>
+        </Row>
+
+       </Grid>
     );
   }
 }
