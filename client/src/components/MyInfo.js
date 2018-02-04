98d8ac4ab5cfdfbbfee6938c0c728081affa6aa9
Kelly <kellyahall.csulb@gmail.com>
2018-02-04 08:25:00 -0800

    removed profile picture, and updated my info to receive values

--- client/src/components/MyInfo.js
+++ client/src/components/MyInfo.js
@@ -8,16 +8,14 @@ const MyInfo = (props) => {
                 <Col xs={12} md={8}>
         <div className = "padcrazy"></div>
         <h1 className = "paddingleft"> My Info </h1>
-        <img classname = "paddingleft profilepicture" src={'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973461_960_720.png'}></img>
-        <div className = "paddingleft">{`First Name`}</div>
-        <div className = "paddingleft">{`Last Name`}</div>
-        <div className = "paddingleft">{`Budget`}</div>
-        <div className = "paddingleft">{`E-mail`}</div>
-        <div className = "paddingleft">{`Calorie Count`}</div>
-        
+        <div className = "paddingleft">{`First Name: Lydia`}</div>
+        <div className = "paddingleft">{`Last Name: Albert`}</div>
+        <div className = "paddingleft">{`Budget: ${localStorage.getItem("budget")}`}</div>
+        <div className = "paddingleft">{`E-mail: lyalbert2018@gmail.com`}</div>
+        <div className = "paddingleft">{`Calorie Count: ${localStorage.getItem("calorie")}`}</div>
         </Col>
     </Row>
-    </Grid> 
+    </Grid>
     )
 }
 
