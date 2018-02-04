ae15a1eed20ad2cdaf2d30d64a284e100f9f77c1
Kelly <kellyahall.csulb@gmail.com>
2018-02-04 08:31:15 -0800

    added a conditional so a blank calorie or budget input will not output null

--- client/src/components/MyInfo.js
+++ client/src/components/MyInfo.js
@@ -10,16 +10,14 @@ const MyInfo = (props) => {
         <h1 className = "paddingleft"> My Info </h1>
         <div className = "paddingleft">{`First Name: Lydia`}</div>
         <div className = "paddingleft">{`Last Name: Albert`}</div>
-        <div className = "paddingleft">{`Budget: ${localStorage.getItem("budget")}`}</div>
+        <div className = "paddingleft">{`Budget: ${localStorage.getItem("budget") === null ? 0: localStorage.getItem("budget")}`}</div>
         <div className = "paddingleft">{`E-mail: lyalbert2018@gmail.com`}</div>
-        <div className = "paddingleft">{`Calorie Count: ${localStorage.getItem("calorie")}`}</div>
+        <div className = "paddingleft">{`Calorie Count: ${localStorage.getItem("calorie") === null ? 0: localStorage.getItem("calorie")}`}</div>
         </Col>
     </Row>
     </Grid>
     )
 }
-
-
 
 export default MyInfo;
 
