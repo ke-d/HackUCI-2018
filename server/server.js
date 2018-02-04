6c1e9c4297d15d51a397db4a322a5731c3c6ac18
amrey9689 <amrey@cpp.edu>
2018-02-03 16:24:59 -0800

    "fetched values from yelp API"

--- server/server.js
+++ server/server.js
@@ -2,61 +2,79 @@ var express = require("express");
 var app = express();
 var cors = require('cors');
 var request = require('request');
-
+const yelp = require('yelp-fusion');
+const client = yelp.client('Ic0tWvEOzB9fGAUPFeL4y9hQv6jbDd7ACqSy5-2bI9JGLH92qNCOuLRt203kcO7pETQWjQGGEAKLINaHCyd284LUEn3SkdMb0iUHotj3lQ0h11cQD3YsGkSrsWB1WnYx');
 app.use(cors());
-const testData = {
-  "total": 8228,
-  "businesses": [
-    {
-      "rating": 4,
-      "price": "$",
-      "phone": "+14152520800",
-      "id": "four-barrel-coffee-san-francisco",
-      "is_closed": false,
-      "categories": [
-        {
-          "alias": "coffee",
-          "title": "Coffee & Tea"
-        }
-      ],
-      "review_count": 1738,
-      "name": "Four Barrel Coffee",
-      "url": "https://www.yelp.com/biz/four-barrel-coffee-san-francisco",
-      "coordinates": {
-        "latitude": 37.7670169511878,
-        "longitude": -122.42184275
-      },
-      "image_url": "http://s3-media2.fl.yelpcdn.com/bphoto/MmgtASP3l_t4tPCL1iAsCg/o.jpg",
-      "location": {
-        "city": "San Francisco",
-        "country": "US",
-        "address2": "",
-        "address3": "",
-        "state": "CA",
-        "address1": "375 Valencia St",
-        "zip_code": "94103"
-      },
-      "distance": 1604.23,
-      "transactions": ["pickup", "delivery"]
-    },
-    // ...
-  ],
-  "region": {
-    "center": {
-      "latitude": 37.767413217936834,
-      "longitude": -122.42820739746094
-    }
-  }
-};
 
-app.get("/testData", (req, res) => {
-  request({url: "https://api.yelp.com/v3/businesses/search?location=90802&results=restaurants", headers: {"Access-Control-Allow-Origin": "*", Authorization: `Bearer Ic0tWvEOzB9fGAUPFeL4y9hQv6jbDd7ACqSy5-2bI9JGLH92qNCOuLRt203kcO7pETQWjQGGEAKLINaHCyd284LUEn3SkdMb0iUHotj3lQ0h11cQD3YsGkSrsWB1WnYx`}}, (error, response, body) => {
-    if (!error && response.statusCode == 200) {
-      console.log(body);
-      res.send(body);
+//User Input
+var latitude = 33.648803;
+var longitude = -117.842813;
+var price = 1;
+
+var user = {
+  favorites: ['Cha For Tea', 'ImpaCafe'],
+}
+  
+  var output = [];
+  var prioritylist = [];
+
+  client.search({
+    latitude: latitude,
+    longitude: longitude,
+    categories: 'food,All',
+    price: price,
+    sort_by: 'distance',
+    radius: 9656, //6 miles
+    open_now: 'false'
+  }).then(response=>{
+    for(i = 0; i<10; i++){
+      //console.log(response.jsonBody.businesses[i].name);
+      
+      output.push(response.jsonBody.businesses[i]);
+      for(i2 = 0; i2< (user.favorites.length);i2++){
+        
+        if(response.jsonBody.businesses[i].name == user.favorites[i2]){
+          prioritylist.push(response.jsonBody.businesses[i]);
+          
+        }
+      }    
+      
+    }
+    //console.log('PRIORITY');
+    for(i = 0; i<prioritylist.length;i++){
+      //console.log(prioritylist[i].name);
+    }
+    //console.log('NEW LIST');
+    var add;
+    for(i = 0; i<(output.length);i++){
+      add = true;
+      for(i2 = 0; i2 < (prioritylist.length); i2++){
+        
+        if ((output[i] == prioritylist[i2]) || ((prioritylist.length) >= 10) ){
+          add = false;
+        }
+      }
+      if (add == true){
+        prioritylist.push(output[i]);
+      }
+    }
+    for(i = 0; i<10; i++){
+      console.log(prioritylist[i].name);
+      console.log(prioritylist[i].price);
+      console.log(prioritylist[i].rating);
+      console.log(prioritylist[i].distance);
+      console.log(prioritylist[i].image_url);
+      
     }
   })
+  
+  
 
-});
+
+
+
+
+app.get('/result',(req,res) => res.send(prioritylist.name))
+
 
 app.listen(8080);
