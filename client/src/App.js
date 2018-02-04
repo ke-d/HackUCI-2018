07ca6963bf979001bd1a5bd656fe290ec3ccaeae
Kenny Do <contact@kennydo.com>
2018-02-04 00:40:05 -0800

    Favorites feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Business from "./components/Business.js";
 import NavBarHeader from "./navigation/NavBarHeader.js";
--- client/src/components/Business.js
+++ client/src/components/Business.js
@@ -1,30 +1,44 @@
 import React from 'react';
 import { ListGroupItem } from 'react-bootstrap';
+import Heart from "../img/heart.png";
+import RedHeart from "../img/heart_red.png";
+class Business extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorite: false
+    }
+  }
 
-const Business = (props) => {
-  const {businesses, index} = props;
-  return (
-    <div>
-    <h1 classname = "midadjust"> Places Near You </h1>
-    <span>{index+1}</span>  
-    
-    <ListGroupItem header={businesses.name}>
-      <ul className ="flex-container">
-      <a href = {businesses.url}>
-      <img className = "flex-item" src={businesses.image_url}/>
-      </a>
-      <div className = "paddingtop">
-        <div className = " leftadjust" >{`Rating: ${businesses.rating}`}</div>
-        <div className = " leftadjust">{`Price: ${businesses.price}`}</div>
-        <div>{`Location: ${businesses.location.address1} ${businesses.location.city}, ${businesses.location.state}`}</div>
-        <div className = "leftadjust">{`Distance: ${Math.round(businesses.distance * 3.28084)} Feet`}</div>
-        <img className = "favoriteimage favoritemargin "src={`https://image.flaticon.com/icons/png/128/126/126471.png`}/>
+  toggleFavorite() {
+    this.setState({favorite: !this.state.favorite});
+  }
+  render() {
+    const {businesses, index} = this.props;
+    console.log(this.state.favorite);
+    return (
+      <div>
+      <h1 classname = "midadjust"> Places Near You </h1>
+      <span>{index+1}</span>
+
+      <ListGroupItem header={businesses.name}>
+        <ul className ="flex-container">
+        <a href = {businesses.url}>
+        <img className = "flex-item" src={businesses.image_url}/>
+        </a>
+        <div className = "paddingtop">
+          <div className = " leftadjust" >{`Rating: ${businesses.rating}`}</div>
+          <div className = " leftadjust">{`Price: ${businesses.price}`}</div>
+          <div>{`Location: ${businesses.location.address1} ${businesses.location.city}, ${businesses.location.state}`}</div>
+          <div className = "leftadjust">{`Distance: ${Math.round(businesses.distance * 3.28084)} Feet`}</div>
+          <img onClick={() => this.toggleFavorite()} className = "favoriteimage favoritemargin canClick" src={this.state.favorite ? RedHeart : Heart}/>
+        </div>
+        </ul>
+
+      </ListGroupItem>
       </div>
-      </ul>
-    
-    </ListGroupItem>
-    </div>
-  )
+    )
+  }
 }
 
 export default Business;
